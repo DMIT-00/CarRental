cf059e22f606e53d2a75fd432e8f43a702288089
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2022-12-23 15:13:22 +0300

    Adding/updating car images support

---
+++ web/src/main/java/com/dmit/controller/add/AddImagesController.java
@@ -0,0 +1,55 @@
+package com.dmit.controller.add;
+
+import com.dmit.entity.car.Image;
+import com.dmit.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+// TODO: is SessionAttributes ok?
+@Controller
+@SessionAttributes("carId")
+public class AddImagesController {
+    @Autowired
+    CarService carService;
+
+
+    // FIXME:: call security! Only car owner can do that!
+    @GetMapping(value = "add-images")
+    public String addCarImagesForm(Model model) {
+        return "add/add_images";
+    }
+
+    // FIXME:: call security! Only car owner can do that!
+    @PostMapping(value = "add-images")
+    public String addCarImages(@ModelAttribute("images") MultipartFile[] files, Model model) {
+
+        List<Image> images = new ArrayList<>();
+
+        Arrays.stream(files)
+                .filter(Predicate.not(MultipartFile::isEmpty))
+                .forEach(f -> {
+                    try {
+                        images.add(new Image(null, f.getBytes()));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+        carService.updateCarImages((UUID) model.getAttribute("carId"), images);
+
+        return "add/add_images";
+    }
+}
---
+++ web/src/main/webapp/jsp/add/add_images.jsp
@@ -0,0 +1,34 @@
+<%@ include file="../templates/header.jsp" %>
+
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+
+<form:form method="POST" action="/CarRental/add-images" enctype="multipart/form-data">
+    <div class="container mt-4 col-md-12">
+        <div class="row">
+            <div class="col-sm">
+                <input type="file" name="files" class="form-control">
+            </div>
+            <div class="col-sm">
+                <input type="file" name="files" class="form-control">
+            </div>
+            <div class="col-sm">
+                <input type="file" name="files" class="form-control">
+            </div>
+            <div class="col-sm">
+                <input type="file" name="files" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-4 col-md-12">
+        <div class="row">
+            <div class="col text-center">
+                <button type="submit" style="width:100%;" name="submit-car" class="btn btn-primary btn-lg btn-block">Add images</button>
+            </div>
+        </div>
+    </div>
+
+
+</form:form>
+
+<%@ include file="../templates/footer.jsp" %>
