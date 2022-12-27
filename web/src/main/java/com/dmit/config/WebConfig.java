047544596e2845539ce25ee4267861d43ceca232
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2022-12-27 14:02:44 +0300

    Added static handler for css resources

--- web/src/main/java/com/dmit/config/WebConfig.java
+++ web/src/main/java/com/dmit/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -35,4 +36,10 @@ public class WebConfig implements WebMvcConfigurer {
         return multipartResolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+    }
 }
