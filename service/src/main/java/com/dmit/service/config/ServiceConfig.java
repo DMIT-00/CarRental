43895aca7d11c2b9a2bcacd1604e370a83da00d9
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2022-12-27 04:00:27 +0300

    Fix service duplicate configuration

--- service/src/main/java/com/dmit/service/config/ServiceConfig.java
+++ service/src/main/java/com/dmit/service/config/ServiceConfig.java
@@ -1,9 +1,16 @@
 package com.dmit.service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan(basePackages = "com.dmit.service")
 public class ServiceConfig {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
