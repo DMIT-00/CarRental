71ff85898e31afff9ad74f44117c17fd41cb7678
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2022-12-27 04:00:54 +0300

    Added tests for password custom constraint

--- web/src/test/java/com/dmit/dto/user/UserRequestDtoTest.java
+++ web/src/test/java/com/dmit/dto/user/UserRequestDtoTest.java
@@ -3,6 +3,7 @@ package com.dmit.dto.user;
 import com.dmit.config.DataConfig;
 import com.dmit.configuration.WebConfiguration;
 import com.dmit.service.config.ServiceConfig;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,58 +22,61 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(SpringRunner.class)
-//@ContextConfiguration(classes = {WebConfiguration.class, ServiceConfig.class, DataConfig.class})
-//@WebAppConfiguration
-//public class UserRequestDtoTest {
-//    @Autowired
-//    protected WebApplicationContext wac;
-//    private Validator validator;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-//        validator = validatorFactory.getValidator();
-//    }
-//
-//    @Test
-//    public void shouldMarkPasswordsAsInvalid() throws Exception {
-//        // Given
-//        UserRequestDto userRequestDto = new UserRequestDto(
-//                "aaaa@gmail.com", "Abram",
-//                null, null,
-//                new UserDetailDto(
-//                        "Abram", "Damk",
-//                        "+37544222222", "0000000000000000",
-//                        Date.valueOf("2000-01-01")));
-//
-//        userRequestDto.setPassword("AAAAAAAA");
-//        userRequestDto.setPasswordRepeat("BBBBBBBB");
-//
-//        // When
-//        Set<ConstraintViolation<UserRequestDto>> violations = validator.validate(userRequestDto);
-//
-//        // Then
-//        assertEquals(1, violations.size());
-//    }
-//
-//    @Test
-//    public void shouldMarkPasswordsAsValid() throws Exception {
-//        // Given
-//        UserRequestDto userRequestDto = new UserRequestDto(
-//                "aaaa@gmail.com", "Abram",
-//                null, null,
-//                new UserDetailDto(
-//                        "Abram", "Damk",
-//                        "+37544222222", "0000000000000000",
-//                        Date.valueOf("2000-01-01")));
-//        userRequestDto.setPassword("AAAAAAAA");
-//        userRequestDto.setPasswordRepeat("AAAAAAAA");
-//
-//        // When
-//        Set<ConstraintViolation<UserRequestDto>> violations = validator.validate(userRequestDto);
-//
-//        // Then
-//        assertEquals(0, violations.size());
-//    }
-//}
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {
+        WebConfiguration.class,
+        ServiceConfig.class,
+        DataConfig.class
+})
+public class UserRequestDtoTest {
+    private Validator validator;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+        validatorFactory.close();
+    }
+
+    @Test
+    public void shouldMarkPasswordsAsInvalid() throws Exception {
+        // Given
+        UserRequestDto userRequestDto = new UserRequestDto(
+                "aaaa@gmail.com", "Abram",
+                null, null,
+                new UserDetailDto(
+                        "Abram", "Damk",
+                        "+37544222222", "0000000000000000",
+                        Date.valueOf("2000-01-01")));
+
+        userRequestDto.setPassword("AAAAAAAA");
+        userRequestDto.setPasswordRepeat("BBBBBBBB");
+
+        // When
+        Set<ConstraintViolation<UserRequestDto>> violations = validator.validate(userRequestDto);
+
+        // Then
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void shouldMarkPasswordsAsValid() throws Exception {
+        // Given
+        UserRequestDto userRequestDto = new UserRequestDto(
+                "aaaa@gmail.com", "Abram",
+                null, null,
+                new UserDetailDto(
+                        "Abram", "Damk",
+                        "+37544222222", "0000000000000000",
+                        Date.valueOf("2000-01-01")));
+        userRequestDto.setPassword("AAAAAAAA");
+        userRequestDto.setPasswordRepeat("AAAAAAAA");
+
+        // When
+        Set<ConstraintViolation<UserRequestDto>> violations = validator.validate(userRequestDto);
+
+        // Then
+        assertEquals(0, violations.size());
+    }
+}
