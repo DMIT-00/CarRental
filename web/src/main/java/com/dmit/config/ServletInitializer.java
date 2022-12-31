3d664ba37ab3a91a2c47aa1f00d27e7f9205fc92
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2022-12-31 16:00:09 +0300

    Changed servlet initialization method

---
+++ web/src/main/java/com/dmit/config/ServletInitializer.java
@@ -0,0 +1,25 @@
+package com.dmit.config;
+
+import com.dmit.service.config.ServiceConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Initialize the Servlet container. This class is detected by the Servlet
+ * container on startup.
+ */
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { DataConfig.class, ServiceConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
