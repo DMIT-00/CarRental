1d5a4614a66aac5b1520b3b2193b48ad2ef2b862
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-06 20:54:30 +0300

    Manual script changes

--- .sh/carrental.sh
+++ .sh/carrental.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-brand?brandName=BMW'
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-brand?brandName=KIA'
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-brand?brandName=Honda'
+curl --request POST --url 'http://localhost:8080/CarRental/add-brand?brandName=BMW'
+curl --request POST --url 'http://localhost:8080/CarRental/add-brand?brandName=KIA'
+curl --request POST --url 'http://localhost:8080/CarRental/add-brand?brandName=Honda'
 
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=1&modelName=X1'
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=1&modelName=X3'
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=1&modelName=X5'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=1&modelName=X1'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=1&modelName=X3'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=1&modelName=X5'
 
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=2&modelName=Sportage'
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=2&modelName=Sorento'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=2&modelName=Sportage'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=2&modelName=Sorento'
 
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=3&modelName=Accord'
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=3&modelName=CR-V'
-curl -d username=root -d password=root -L http://localhost:8080/CarRental/user-login --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=3&modelName=Civic'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=3&modelName=Accord'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=3&modelName=CR-V'
+curl --request POST --url 'http://localhost:8080/CarRental/add-model?carBrand.id=3&modelName=Civic'
