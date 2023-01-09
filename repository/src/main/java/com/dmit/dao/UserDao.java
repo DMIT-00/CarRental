3fa34fbf578f6a0c433fd537f7df09187538c3b6
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-09 18:29:19 +0300

    New methods in UserDao

--- repository/src/main/java/com/dmit/dao/UserDao.java
+++ repository/src/main/java/com/dmit/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.dmit.dao;
 
 import com.dmit.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,13 @@ public interface UserDao extends JpaRepository<User, UUID> {
     User findByUsername(String username);
     User findByUserDetail_CreditCard(String creditCard);
     User findByUserDetail_PhoneNumber(String phoneNumber);
+    long countByLockedTrue();
+    long countByLockedFalse();
+    long countByActiveOrderNotNull();
+    long countByActiveOrderIsNull();
+
+    Page<User> findAllByLockedTrue(Pageable pageable);
+    Page<User> findAllByLockedFalse(Pageable pageable);
+    Page<User> findAllByActiveOrderNotNull(Pageable pageable);
+    Page<User> findAllByActiveOrderIsNull(Pageable pageable);
 }
