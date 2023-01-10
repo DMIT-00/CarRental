63d3737bd9ea7440bccef03a044fbe5fe7acd7e5
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-10 03:53:39 +0300

    New methods in OrderDao

--- repository/src/main/java/com/dmit/dao/OrderDao.java
+++ repository/src/main/java/com/dmit/dao/OrderDao.java
@@ -1,9 +1,14 @@
 package com.dmit.dao;
 
 import com.dmit.entity.order.Order;
+import com.dmit.entity.order.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface OrderDao extends JpaRepository<Order, UUID> {
+    long countByOrderStatus(OrderStatus orderStatus);
+    Page<Order> findAllByOrderStatus(OrderStatus orderStatus, Pageable pageable);
 }
