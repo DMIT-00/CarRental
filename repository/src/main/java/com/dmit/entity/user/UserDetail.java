c698a7b750fab4c1b69df4c3a0a4e788ad409ef6
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-10 03:52:33 +0300

    Added lombok missed annotations to UserDetail

--- repository/src/main/java/com/dmit/entity/user/UserDetail.java
+++ repository/src/main/java/com/dmit/entity/user/UserDetail.java
@@ -1,6 +1,8 @@
 package com.dmit.entity.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
 
@@ -12,6 +14,8 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "t_user_detail")
 public class UserDetail {
     @Id
