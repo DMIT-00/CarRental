c6644accd864361508defd7b278b1c77e56bad94
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-20 00:00:19 +0300

    Added data.sql to populate database at startup with user roles and default admin and manager

--- repository/src/main/java/com/dmit/config/DataConfig.java
+++ repository/src/main/java/com/dmit/config/DataConfig.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -79,6 +82,17 @@ public class DataConfig {
         factory.afterPropertiesSet();
 
         return factory.getObject();
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource("/data.sql"));
+
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(dataSource);
+        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+        return dataSourceInitializer;
     }
 
     @Bean
---
+++ repository/src/main/resources/data.sql
@@ -0,0 +1,21 @@
+-- Adding default user roles
+INSERT IGNORE INTO t_role (role_id, role_name) VALUES (1, 'ADMIN');
+INSERT IGNORE INTO t_role (role_id, role_name) VALUES (2, 'MANAGER');
+INSERT IGNORE INTO t_role (role_id, role_name) VALUES (3, 'USER');
+
+-- Adding root
+INSERT IGNORE INTO t_user (user_id, user_email, user_locked, user_password, user_username)
+VALUES ('00000000-0000-0000-0000-000000000001', 'root@root.com', false, '$2a$10$cBtsPltbJbv8rf67d2IUTO2NuE7MrMQIgDadE5HWGNXgSecVp7I1W', 'root');
+INSERT IGNORE INTO t_user_detail (user_detail_id, user_birth_date, user_credit_card, user_first_name, user_last_name, user_phoneNumber)
+VALUES ('00000000-0000-0000-0000-000000000001', '0008-08-08', '0000000000000001', 'Root', 'The Root', '+880000001');
+INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-0000-000000000001', '1');
+INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-0000-000000000001', '2');
+INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-0000-000000000001', '3');
+
+-- Adding manager
+INSERT IGNORE INTO t_user (user_id, user_email, user_locked, user_password, user_username)
+VALUES ('00000000-0000-0000-0000-000000000002', 'manager@manager.com', false, '$2a$10$Focn3iN1TJjCq1bdlI/TrO7WADqf6zYqH5CC9WyPALBq/gkmGtVGq', 'manager');
+INSERT IGNORE INTO t_user_detail (user_detail_id, user_birth_date, user_credit_card, user_first_name, user_last_name, user_phoneNumber)
+VALUES ('00000000-0000-0000-0000-000000000002', '0008-08-08', '0000000000000002', 'Manager', 'The Manager', '+880000002');
+INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-0000-000000000002', '2');
+INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-0000-000000000002', '3');
