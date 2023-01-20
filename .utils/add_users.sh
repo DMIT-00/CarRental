0c2d5c28bd50d43899cd61bf3d768ffd36754b5d
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-20 17:04:29 +0300

    Removed root and manager from add_users.sh, added 2 new users

--- .utils/add_users.sh
+++ .utils/add_users.sh
@@ -1,34 +1,6 @@
 #!/bin/sh
 curl --request POST --url http://localhost:8080/CarRentalRest/api/v1/users --header 'Content-Type: application/json' \
   --data '{
-	"email": "root@gmail.com",
-	"username": "root",
-	"password": "root",
-	"userDetail": {
-		"firstName": "Root",
-		"lastName": "The Root",
-		"phoneNumber": "+880000000",
-		"creditCard": "0000000000000000",
-		"birthDate": -61895934000000
-	}
-}'
-
-curl --request POST --url http://localhost:8080/CarRentalRest/api/v1/users --header 'Content-Type: application/json' \
-  --data '{
-	"email": "manager@gmail.com",
-	"username": "manager",
-	"password": "manager",
-	"userDetail": {
-		"firstName": "Manager",
-		"lastName": "The Manager",
-		"phoneNumber": "+880000001",
-		"creditCard": "0000000000000001",
-		"birthDate": -61895934000000
-	}
-}'
-
-curl --request POST --url http://localhost:8080/CarRentalRest/api/v1/users --header 'Content-Type: application/json' \
-  --data '{
 	"email": "user@gmail.com",
 	"username": "user",
 	"password": "user",
@@ -37,6 +9,34 @@ curl --request POST --url http://localhost:8080/CarRentalRest/api/v1/users --hea
 		"lastName": "The User",
 		"phoneNumber": "+880000002",
 		"creditCard": "0000000000000002",
+		"birthDate": -61895934000000
+	}
+}'
+
+curl --request POST --url http://localhost:8080/CarRentalRest/api/v1/users --header 'Content-Type: application/json' \
+  --data '{
+	"email": "alex@gmail.com",
+	"username": "Alex",
+	"password": "Alex",
+	"userDetail": {
+		"firstName": "Alex",
+		"lastName": "The Alex",
+		"phoneNumber": "+880000004",
+		"creditCard": "0000000000000202",
+		"birthDate": -61895934000000
+	}
+}'
+
+curl --request POST --url http://localhost:8080/CarRentalRest/api/v1/users --header 'Content-Type: application/json' \
+  --data '{
+	"email": "emily08@gmail.com",
+	"username": "Emily",
+	"password": "Emily",
+	"userDetail": {
+		"firstName": "Emily",
+		"lastName": "The Emily",
+		"phoneNumber": "+880000204",
+		"creditCard": "0000004000030202",
 		"birthDate": -61895934000000
 	}
 }'
