869d31403b968dce93d1487578a617c3628ad84c
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-21 21:31:42 +0300

    Added more comments to data.sql, changed name of thread in jMeter saved configuration

--- repository/src/main/resources/data.sql
+++ repository/src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT IGNORE INTO t_role (role_id, role_name) VALUES (1, 'ADMIN');
 INSERT IGNORE INTO t_role (role_id, role_name) VALUES (2, 'MANAGER');
 INSERT IGNORE INTO t_role (role_id, role_name) VALUES (3, 'USER');
 
--- Adding root
+-- Adding "root" user with "root" password
 INSERT IGNORE INTO t_user (user_id, user_email, user_locked, user_password, user_username)
 VALUES ('00000000-0000-0000-0000-000000000001', 'root@root.com', false, '$2a$10$cBtsPltbJbv8rf67d2IUTO2NuE7MrMQIgDadE5HWGNXgSecVp7I1W', 'root');
 INSERT IGNORE INTO t_user_detail (user_detail_id, user_birth_date, user_credit_card, user_first_name, user_last_name, user_phoneNumber)
@@ -12,7 +12,7 @@ INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-00
 INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-0000-000000000001', '2');
 INSERT IGNORE INTO t_user_role (user_id, role_id) VALUES ('00000000-0000-0000-0000-000000000001', '3');
 
--- Adding manager
+-- Adding "manager" user with "manager" password
 INSERT IGNORE INTO t_user (user_id, user_email, user_locked, user_password, user_username)
 VALUES ('00000000-0000-0000-0000-000000000002', 'manager@manager.com', false, '$2a$10$Focn3iN1TJjCq1bdlI/TrO7WADqf6zYqH5CC9WyPALBq/gkmGtVGq', 'manager');
 INSERT IGNORE INTO t_user_detail (user_detail_id, user_birth_date, user_credit_card, user_first_name, user_last_name, user_phoneNumber)
