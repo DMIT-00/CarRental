3dfd84f9487e6ef3c377abb13db8a82dc76e19b6
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-01-27 19:58:34 +0300

    Updated exception handler

--- web/src/main/java/com/dmit/controller/WebExceptionHandler.java
+++ web/src/main/java/com/dmit/controller/WebExceptionHandler.java
@@ -3,17 +3,40 @@ package com.dmit.controller;
 import com.dmit.exception.AlreadyExistsException;
 import com.dmit.exception.InvalidOperation;
 import com.dmit.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class WebExceptionHandler {
-    @ExceptionHandler(value = {NotFoundException.class, AlreadyExistsException.class, InvalidOperation.class})
-    public String exceptionHandler(Model model, Exception exception) {
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = {
+            NotFoundException.class,
+            AlreadyExistsException.class,
+            InvalidOperation.class
+    })
+    public String customExceptionHandler(Model model, Exception exception) {
         model.addAttribute("exceptionCaption", exception.getClass().getSimpleName());
         model.addAttribute("exceptionBody", exception.getMessage());
 
         return "exception_box";
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public String globalExceptionHandler(Model model, Exception exception) {
+        model.addAttribute("exceptionCaption", exception.getClass().getSimpleName());
+        model.addAttribute("exceptionBody", chainedString(exception));
+
+        return "exception_box";
+    }
+
+    public static String chainedString(Throwable throwable) {
+        StringBuilder SB = new StringBuilder(throwable.toString());
+        while((throwable = throwable.getCause()) != null)
+            SB.append("<br>caused by ").append(throwable);
+        return SB.toString();
     }
 }
