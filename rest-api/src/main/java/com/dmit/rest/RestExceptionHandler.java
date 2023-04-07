8e239e2772332f396e49b89e7a633d582de01517
DMIT-00 <114758136+DMIT-00@users.noreply.github.com>
2023-04-07 14:06:15 +0300

    Added ConstraintViolationException exception handler in rest-api

--- rest-api/src/main/java/com/dmit/rest/RestExceptionHandler.java
+++ rest-api/src/main/java/com/dmit/rest/RestExceptionHandler.java
@@ -6,11 +6,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> handleException(NotFoundException exception)
     {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleException(ConstraintViolationException exception)
+    {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
